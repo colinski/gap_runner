79f2d606504e661f7e3a912f7de2c80fd079aef9
Colin <csamplawski@cs.umass.edu>
2022-04-06 18:54:16 -0400

    added scripts to test latency of onnxruntime

---
+++ nano/test.py
@@ -0,0 +1,20 @@
+import onnxruntime as ort
+import numpy as np
+from tqdm import tqdm, trange
+import time
+
+
+model = '/root/gap_runner/nano/suffix.onnx'
+sess = ort.InferenceSession(model, providers=['CPUExecutionProvider'])
+#sess = ort.InferenceSession(model, providers=['CUDAExecutionProvider'])
+# sess = ort.InferenceSession(model, providers=['TensorrtExecutionProvider'])
+
+img = np.zeros((1, 32, 28, 28), dtype=np.float32)
+
+input_name = sess.get_inputs()[0].name
+
+for i in range(1000):
+    start = time.time()
+    outputs = sess.run([], {input_name: img})[0]
+    esp_time = time.time() - start
+    print(i, esp_time*1000)
